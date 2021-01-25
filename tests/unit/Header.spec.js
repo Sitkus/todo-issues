3aade67fdef20fad80623a1f684f24beee2e9d7d
Sitkus <65811246+Sitkus@users.noreply.github.com>
2021-01-25 13:31:45 +0200

    Added a test to see if logo is appearing in Header

--- tests/unit/Header.spec.js
+++ tests/unit/Header.spec.js
@@ -14,7 +14,13 @@ afterEach(() => {
 });
 
 describe('Header', () => {
-  it('renders the navigation links', () => {
+  it('Check that logo exists', () => {
+    const img = wrapper.findAll('.nav__logo');
+
+    expect(img.length).toBe(1);
+  });
+
+  it('Renders the navigation links', () => {
     const navLinks = wrapper.findAll('.nav__link');
 
     expect(navLinks.length).toBe(4);
